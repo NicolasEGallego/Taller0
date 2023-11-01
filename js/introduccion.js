9c862c5fe4a22195e1de9312a732ee294b1337c3
nicolas <>
2023-11-01 12:56:12 -0500

    .

---
+++ js/introduccion.js
@@ -0,0 +1,35 @@
+//variable
+//$name
+//_name
+//name
+//nombrePersona
+
+let numero=1;
+let nombre='Tatiana';
+let miVariable=true;
+let miVariable1=[1,'bob'];
+//let suma =num1+num2;
+
+pi = 3.1416;
+
+//const pi
+//console.log('el numero 1 ${num1} es muy bonito\n' + 'jjjj')
+
+
+let a=5;
+let b=10;
+console.log(`la suma es ${a+b}`)
+/*operadores
+aritmeticos + - * / mod %
+de comparacion == === != !==
+< > <= >=
+lógicos && || !
+console.info()
+console.warn()
+console.error()
+console.clear()
+*/
+
+let num1= parseInt(prompt("digite el primer número"))
+let num2= parseInt(prompt("digite el segundo número"))
+console.log(`la suma de num1 y num2 es ${num1+num2}`)
